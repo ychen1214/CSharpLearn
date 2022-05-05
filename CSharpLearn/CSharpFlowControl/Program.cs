49bce087e3d90a2a491a921cb0cdb9b18e5cea18
Yu <ychen1214@users.noreply.github.com>
2022-05-05 08:54:29 -0400

    added new projects for different types of CSharp learning

---
+++ CSharpLearn/CSharpFlowControl/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFlowControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 12;
+            if (number < 5)
+            {
+                Console.WriteLine("{0} is less than 5", number);
+            }
+            else if (number > 5)
+            {
+                Console.WriteLine("{0} is greater than or equal to 5",number);
+                if (number < 10)
+                {
+                    Console.WriteLine("{0} proper language learning", number);
+                }
+                else
+                {
+                    Console.WriteLine("{0} improper language learning", number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("{0} is equal to 5",number);
+            }
+            Console.WriteLine("This statement is always execuated.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpLearn/CSharpIO/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testString;
+            Console.Write("Enter a string -");
+            testString = Console.ReadLine();
+            Console.WriteLine("You entered '{0}'", testString);
+            Console.ReadLine();
+        }
+    }
+}
