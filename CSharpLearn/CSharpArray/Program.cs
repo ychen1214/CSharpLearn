bda08c092cd982bdd0d2767716b4a79de7dbcdf2
Yu <ychen1214@users.noreply.github.com>
2022-05-12 08:13:34 -0400

    ARRAYS

--- CSharpLearn/CSharpArray/Program.cs
+++ CSharpLearn/CSharpArray/Program.cs
@@ -10,6 +10,25 @@ namespace CSharpArray
     {
         static void Main(string[] args)
         {
+            int[] age = new int[5];
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            age[0] = 12;
+            age[1] = 4;
+            age[2] = 5;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine("Element in first index : " + numbers[i]);
+            }
+
+            //some linq
+            Console.WriteLine("Smallest Element: " + numbers.Min());
+            Console.WriteLine("Smallest Element: " + numbers.Max());
+            Console.WriteLine("Smallest Element: " + numbers.Average());
+            Console.ReadLine();
+
         }
     }
 }
